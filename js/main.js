45088445efa0d6b6b14a9b7e7590efd875bb5b34
sayda <sayda>
2017-03-13 13:20:09 -0500

    validano con

--- js/main.js
+++ js/main.js
@@ -1,25 +1,30 @@
-$(document).ready(function(){
+$(document).ready(init);
+
+function init(){
     $('#boton1').click(function(){
-        $('#bienvenido').hide();
-        $('#nombres').show(); 
-    });
-    
-    $('#boton2').click(function(){
-        var nombre1=$('#nombre').val();
-        var nombre2=$('#nombre2').val();
-        if(nombre1==""||nombre2==""){
-            alert("Complete los campos");
-        }else{
-            $('#nombres').hide();
-            $('#juego').show(); 
-        }
-    });
+        $('#bienvenido').hide(1000);
+        $('#nombres').show(1000); 
+    });    
+
     $('#boton3').click(function (){
         $('#juego').hide(1000); 
         $('#historial').show(1000);
     });
     $('.comentar').click(function(){
-        $('#historial').hide(); 
-       $('#comentarios').show();
+        $('#historial').hide(1000); 
+       $('#comentarios').show(1000);
     });
-})
+}
+//funciones de seciones
+function validarNombres(){
+   $('#boton2').click(function(){
+        var nombre1=$('#nombre').val();
+        var nombre2=$('#nombre2').val();
+        if(nombre1==""||nombre2==""){
+            alert("Complete los campos");
+        }else{
+            $('#nombres').hide(1000);
+            $('#juego').show(1000); 
+        }
+    }); 
+}
