680a2f95934f6bc3ce6d6243fe4b3798fc0cb140
sayda <sayda>
2017-03-14 17:29:27 -0500

    comentarios y tabla

--- js/main.js
+++ js/main.js
@@ -1,40 +1,52 @@
 $(document).ready(init);
 function init(){
-    rotacionSeciones();
+    //ROTACIONES DE LAS SECIONES
+    $('#navegaHistorial').click(naveHistorial);
+    $('#boton1').click(naveNombres);        
+    $('#boton2').click(naveJuego);
+    $('#boton3').click(naveHistorial2);
+    
+    //ver comentarios funcion
+    $('#lista-juegos').on('click','button', naveComentario);
+    
     juegoTablero();
+    solicitarPeticiones();
+    solicitarComentarios();
 }
-
 //ROTACIONES DE LAS SECIONES
-function rotacionSeciones(){
-    $('#boton1').click(function(){
-        $('#bienvenido').hide(1000);
-        $('#nombres').show(1000); 
-    });
-    $('.comentar').click(function(){
-        $('#historial').hide(1000);
-        $('#comentarios').show(1000);        
-    });
-    
-    $('#boton2').click(function(){
-        var nombre1=$('#nombre').val();
-        var nombre2=$('#nombre2').val();
-        if(nombre1==""||nombre2==""){
-            $('#salida').text('Por fabor ingresen sus nombres .').css('color', 'red');
-        }else{
-            $('#nombres').hide(1000);
-            $('#juego').show(1000); 
-        }
-        validarNombres();
-    });
-    
-    $('#boton3').click(function (){
-        $('#juego').hide(1000); 
-        $('#historial').show(1000);
-        solicitar();
-    });
-    $('#lista-juegos').on('click','li','button' onBtnItemJuego)
+function naveHistorial(){
+    $('#bienvenido').hide(1000); 
+    $('#historial').show(1000);
+    solicitarPeticiones(); 
 }
-
+function naveHistorial2(){
+    $('#juego').hide(1000); 
+    $('#historial').show(1000);
+    solicitarPeticiones();
+}
+function naveNombres(){
+    $('#bienvenido').hide(1000);
+    $('#nombres').show(1000); 
+}
+function naveJuego(){
+    var nombre1=$('#nombre').val();
+    var nombre2=$('#nombre2').val();
+    if(nombre1==""||nombre2==""){
+        $('#salida').text('Por fabor ingresen sus nombres .').css('color', 'red');
+    }else{
+        $('#nombres').hide(1000);
+        $('#juego').show(1000); 
+    }
+    validarNombres();
+}
+var gameID;//currentGameID
+function naveComentario(){
+    var idGame=$(this).parent().data('idgame');
+    console.log(_idGame);
+    solicitarComentarios(_idGame);
+    $('#historial').hide(1000);
+    $('#comentarios').show(1000);
+}
 //VALIDANDO EL ENVIO DE NOMBRES
 function validarNombres(){
     var nombre1=$('#nombre').val();
@@ -46,39 +58,66 @@ function validarNombres(){
     saliNombre2.text(nombre2);
 }
 //JUEGO DEL TABLERO
+console.log(juegoTablero);
 function juegoTablero(){
     var juego = new Array(9);
-    var tabla=$('td').each(function(){
-       $(this).click(function(){
-          console.log('funciona..!!');
-       })
-    });
+    var tabla=$('td');
+    for(var i=0;i>9; i++){
+        tabla[i].html("X");
+    }
+    /*.each(function(){
+       $("td")[i].click(function(){
+           $('td').html("X"); 
+       });
+    });*/
 };
 
-function solicitar(){
+//AJAX PETICIONES----- HISTORIAL HTML
+function solicitarPeticiones(){
     $.ajax({url:'http://test-ta.herokuapp.com/games',
     }).done(function (_data){
         console.log(_data);
         dubujarHistorial(_data);
     });
 }
+//HTML HISTORIAL
 function dubujarHistorial(_datos){
-    //var list=$('#lista-juegos');
-    //list.html('hola...!!!');
     var carga='';
     for(var i in _datos){
-      console.log(_datos[i].winner_player);
-      var html ='<li data-idgame="" class="list-group-item"><button class="btn">ver</button>Ganador:'+_datos[i].winner_player+'</li>'
+      //console.log(_datos[i].winner_player);
+      var html ='<li data-idgame="'+_datos[i].id+'" class="list-group-item">'+_datos[i].winner_player+' le gano a '+_datos[i].loser_player+' en '+_datos[i].number_of_turns_to_win+' movimientos <br><button class="btn">ver</button></li>'
         carga+=html;
     }
     $('#lista-juegos').html(carga);
 }
-function onBtnItemJuego(){
-    $.ajax({
-        url:'http://test-ta.herokuapp.com/games/1',
-    }).done(function (){
-        
-    });
+//COMENTARIOS HTML
+function solicitarComentarios(_idGame)
+{
+	$.ajax({
+		url:'http://test-ta.herokuapp.com/games/'+_idGame+'/comments',
+		type:'GET'
+	}).done(function(_data){
+		console.log(_data);
+        dibujarComentarios(_data);
+	});
 }
 
+//HTML COMENTARIOS LISTA
+function dibujarComentarios(_data){
+    var comentaHtml='';
+    for(var i in _data){
+        var html = '<li class="list-group-item">'+_data[i].name+' dice: <p>'+ _data[i].content +'</p></li>';
+        comentaHtml+=html;
+    }
+    $('#lista-comentarios').html(comentaHtml);
+}
 
+/*function enviarComentario (_idgame , _name , _content){
+    $.ajax({
+        url:'http://test-ta.herokuapp.com/games/'+_idgame+'/comments',
+        type:'POST',
+        data:{coment:{name:_name, content:_content, game_id:_idgame}}
+    }).done(function (_data){
+        console.log(_data);
+    });
+}*/
