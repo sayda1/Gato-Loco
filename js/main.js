b092a7efbe551addc88b4a2ef120f1a677dbc350
sayda <sayda>
2017-03-14 09:04:24 -0500

    mensaje del nombre

--- js/main.js
+++ js/main.js
@@ -1,33 +1,32 @@
 $(document).ready(init);
-//funciones de seciones
-function init(){   
-    
+function init(){
+    rotacionSeciones();
 }
 
-//FUNCTION  DE BIENVENIDO A GATO LOCO
-function gatoLoco(){
-    $('#bienvenido').hide(1000);
-    $('#nombres').show(1000); 
+
+//ROTACIONES DE LAS SECIONES
+function rotacionSeciones(){
+     $('#boton1').click(function(){
+        $('#bienvenido').hide(1000);
+        $('#nombres').show(1000); 
+    });
+    
+    $('#boton2').click(function(){
+        var nombre1=$('#nombre').val();
+        var nombre2=$('#nombre2').val();
+        if(nombre1==""||nombre2==""){
+            $('#salida').text('Por fabor ingresen sus nombres .').css('color', 'red');
+        }else{
+            $('#nombres').hide(1000);
+            $('#juego').show(1000); 
+        }
+    });
+    $('#boton3').click(function (){
+        $('#juego').hide(1000); 
+        $('#historial').show(1000);
+    });
+    $('.comentar') .click(function(){
+        $('#historial').hide(1000);
+        $('#comentarios').show(1000);        
+    });
 }
-//FUNCTION  DE INGRESE NOMBRES
-function ingreseNombres(){
-    var nombre1=$('#nombre').val();
-    var nombre2=$('#nombre2').val();
-    if(nombre1==""||nombre2==""){
-        $('#salida').html('Complete los campos .').css('color' , 'red');
-    }else{
-        $('#nombres').hide(1000);            
-        $('#juego').show(1000); 
-    }
-    nombre1
-}
-//FUNCTION MANDAR HISTORIAL
-function mandarHistorial(){
-    $('#juego').hide(1000); 
-    $('#historial').show(1000);
-}
-//FUNCTION  PARA COMENTAR
-$('.comentar').click(function(){
-    $('#historial').hide(1000); 
-    $('#comentarios').show(1000);
-});
