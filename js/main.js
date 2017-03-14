2d26bc80a3d8cd026dd2279ded6c42099719040c
sayda <sayda>
2017-03-14 08:39:18 -0500

    validacion de seciones

--- js/main.js
+++ js/main.js
@@ -1,30 +1,33 @@
 $(document).ready(init);
-
-function init(){
-    $('#boton1').click(function(){
-        $('#bienvenido').hide(1000);
-        $('#nombres').show(1000); 
-    });    
-
-    $('#boton3').click(function (){
-        $('#juego').hide(1000); 
-        $('#historial').show(1000);
-    });
-    $('.comentar').click(function(){
-        $('#historial').hide(1000); 
-       $('#comentarios').show(1000);
-    });
-}
 //funciones de seciones
-function validarNombres(){
-   $('#boton2').click(function(){
-        var nombre1=$('#nombre').val();
-        var nombre2=$('#nombre2').val();
-        if(nombre1==""||nombre2==""){
-            alert("Complete los campos");
-        }else{
-            $('#nombres').hide(1000);
-            $('#juego').show(1000); 
-        }
-    }); 
+function init(){   
+    
 }
+
+//FUNCTION  DE BIENVENIDO A GATO LOCO
+function gatoLoco(){
+    $('#bienvenido').hide(1000);
+    $('#nombres').show(1000); 
+}
+//FUNCTION  DE INGRESE NOMBRES
+function ingreseNombres(){
+    var nombre1=$('#nombre').val();
+    var nombre2=$('#nombre2').val();
+    if(nombre1==""||nombre2==""){
+        $('#salida').html('Complete los campos .').css('color' , 'red');
+    }else{
+        $('#nombres').hide(1000);            
+        $('#juego').show(1000); 
+    }
+    nombre1
+}
+//FUNCTION MANDAR HISTORIAL
+function mandarHistorial(){
+    $('#juego').hide(1000); 
+    $('#historial').show(1000);
+}
+//FUNCTION  PARA COMENTAR
+$('.comentar').click(function(){
+    $('#historial').hide(1000); 
+    $('#comentarios').show(1000);
+});
